7b6cb284a33b6ea5ff032504f3a52ecfd3a2446e
winkleL <NEU8830@163.com>
2020-08-04 01:35:00 +0800

    swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
   <home-header/>
+  <home-swiper/>
+  <div>test</div>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/swiper.vue'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+    <swiper-slide v-for="item of swiperList" :key="item.id"><img class="swiper-img" :src="item.imgurl" alt=""></swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'HomeSwiper',
+    data(){
+        return {
+            swiperOptions:{
+                pagination:'.swiper-pagination'
+            },
+            swiperList:[
+                {
+                    id:'0001',
+                    imgurl:'https://img.dpm.org.cn/Uploads/Picture/2020/07/28/s5f1fe8978bf05.jpg'
+                },
+                {
+                    id:'0002',
+                    imgurl:'https://img.dpm.org.cn/Uploads/Picture/2020/07/31/s5f23afcc67ec3.jpg'
+                }
+            ]
+        }
+    }
+}
+
+</script>
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background-color: #fbeee2
+.wrapper
+  overflow:hidden
+  .swiper-img 
+    height:3rem
+    width:100%
+</style>
